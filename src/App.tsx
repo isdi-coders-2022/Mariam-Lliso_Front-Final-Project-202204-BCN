19d76b0b383d972803c0f00dcc3591f1f7e29f29
MariamLliso <contacto.mariamlliso@gmail.com>
2022-06-07 15:29:21 +0200

    added routes to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import Header from "./components/Layout/Header/Header";
+import Footer from "./components/Layout/Footer/Footer";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import ContainerStyle from "./AppStyle";
+import { Navigate, Route, Routes } from "react-router-dom";
+import EstablishmentListPage from "./pages/EstablishmentListPage/EstablishmentListPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 
 const App = () => {
 
   return (
-  <>
-    <Header></Header>
-    <RegisterPage></RegisterPage>
-  </>
+  <ContainerStyle>
+    <Header/>
+    <main className="main">
+      <Routes>
+        <Route path="/" element={<Navigate to="/establishment/list" />} />
+        <Route path="/establishment/list" element={<EstablishmentListPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </main>
+    <Footer />
+  </ContainerStyle>
   )
 }
 
