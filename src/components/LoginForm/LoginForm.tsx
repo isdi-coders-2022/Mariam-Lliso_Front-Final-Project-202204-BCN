3b4d14d590d7c5e2d79ffb6f2391a2172e211b3a
MariamLliso <contacto.mariamlliso@gmail.com>
2022-06-12 10:22:44 +0200

    formated login and register

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,5 +1,14 @@
-import { FormControl, FormHelperText, IconButton, InputAdornment, InputLabel, Link, OutlinedInput, Stack } from "@mui/material";
-import { useState } from "react";
+import {
+  FormControl,
+  FormHelperText,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  Link,
+  OutlinedInput,
+  Stack,
+} from "@mui/material";
+import { useCallback, useState } from "react";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import { IUserLogin, IValidationUserLogin } from "../../types/userInterfaces";
@@ -8,20 +17,23 @@ import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { userLoginThunk } from "../../redux/thunks/userThunks/userThunks";
 import { useNavigate } from "react-router-dom";
 import { IUserInterface } from "../../types/uiInterfaces";
+import AlertCustom, { IAlertCustom } from "../Layout/AlertCustom/AlertCustom";
+import { feedbackOffActionCreator } from "../../redux/features/uiSlice/uiSlice";
 
 const LoginForm = (): JSX.Element => {
   const dispatch = useAppDispatch();
-  const { loadingUser } = useAppSelector<IUserInterface>((state) => state.ui);
+  const navigate = useNavigate();
+  const { loadingUser, feedback, statusCode } = useAppSelector<IUserInterface>(
+    (state) => state.ui
+  );
 
   const formInitialState: IUserLogin = {
     username: "",
     password: "",
-  }
+  };
   const [formData, setFormData] = useState<IUserLogin>(formInitialState);
 
-  const submitRegisterForm = (
-    event: React.FormEvent<HTMLFormElement>
-  ) => {
+  const submitRegisterForm = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const isNotValidated = validateFields();
     if (isNotValidated) {
@@ -30,49 +42,62 @@ const LoginForm = (): JSX.Element => {
 
     dispatch(userLoginThunk(formData));
     resetData();
+    navigate("/user/profile");
   };
 
   const resetData = (): void => {
     setFormData(formInitialState);
-  }
+  };
 
   const changeData = (event: React.ChangeEvent<HTMLInputElement>): void => {
     setFormData({ ...formData, [event.target.id]: event.target.value });
   };
 
-
   const errorInitialState: IValidationUserLogin = {
     username: false,
     password: false,
-  }
+  };
   const [errors, setErrors] = useState<IValidationUserLogin>(errorInitialState);
 
   const validateFields = (): boolean => {
-    let tempErrors = {...errors};
+    let tempErrors = { ...errors };
     tempErrors.username = formData.username === "" ? true : false;
     tempErrors.password = formData.password === "" ? true : false;
     setErrors(tempErrors);
 
-    return Object.values(tempErrors).some(element => element === true);
-  }
+    return Object.values(tempErrors).some((element) => element === true);
+  };
 
   const [showPassword, setShowPassword] = useState<boolean>(false);
 
   const handleClickShowPassword = () => {
-      setShowPassword(!showPassword);
+    setShowPassword(!showPassword);
   };
 
-  const navigate = useNavigate();
   const navigateToRegister = () => {
     navigate("/register");
+  };
+
+  const feedbackOff = useCallback(
+    () => dispatch(feedbackOffActionCreator()),
+    [dispatch]
+  );
+
+  const validationServer: IAlertCustom = {
+    title: `Usuario o contraseña erroneos`,
+    content: `
+      Comprueba que los datos introducidos son correctos`,
+    type: "error",
+    action: feedbackOff,
   };
 
   return (
     <form autoComplete="off" onSubmit={submitRegisterForm}>
       <Stack spacing={3}>
-
         <FormControl variant="outlined">
-          <InputLabel htmlFor="username" error={errors.username}>Username</InputLabel>
+          <InputLabel htmlFor="username" error={errors.username}>
+            Username
+          </InputLabel>
           <OutlinedInput
             id="username"
             type="text"
@@ -81,11 +106,15 @@ const LoginForm = (): JSX.Element => {
             value={formData.username}
             error={errors.username}
           />
-          <FormHelperText id="username-helpertext" error={errors.username}>{errors.username ? "El nombre de usuario es obligatorio" : " "}</FormHelperText>
+          <FormHelperText id="username-helpertext" error={errors.username}>
+            {errors.username ? "El nombre de usuario es obligatorio" : " "}
+          </FormHelperText>
         </FormControl>
 
         <FormControl variant="outlined">
-          <InputLabel htmlFor="password" error={errors.password}>Password</InputLabel>
+          <InputLabel htmlFor="password" error={errors.password}>
+            Password
+          </InputLabel>
           <OutlinedInput
             id="password"
             role="password"
@@ -106,17 +135,45 @@ const LoginForm = (): JSX.Element => {
             value={formData.password}
             error={errors.password}
           />
-          <FormHelperText id="password-helpertext" error={errors.password}>{errors.password ? "La contraseña es obligatoria" : " "}</FormHelperText>
+          <FormHelperText id="password-helpertext" error={errors.password}>
+            {errors.password ? "La contraseña es obligatoria" : " "}
+          </FormHelperText>
         </FormControl>
 
-        <LoadingButton fullWidth size="large" type="submit" variant="contained" loading={loadingUser}>
+        <LoadingButton
+          fullWidth
+          size="large"
+          type="submit"
+          variant="contained"
+          loading={loadingUser}
+        >
           iniciar sesión
         </LoadingButton>
 
-        <Link variant="subtitle2" type="button" component="button" underline="none" onClick={navigateToRegister}>¿Aun no tienes cuenta? ¡Registrate ahora!</Link>
+        {feedback &&
+          (statusCode === 403 ? (
+            <AlertCustom
+              title={validationServer.title}
+              content={validationServer.content}
+              type={validationServer.type}
+              action={validationServer.action}
+            ></AlertCustom>
+          ) : (
+            ""
+          ))}
+
+        <Link
+          variant="subtitle2"
+          type="button"
+          component="button"
+          underline="none"
+          onClick={navigateToRegister}
+        >
+          ¿Aun no tienes cuenta? ¡Registrate ahora!
+        </Link>
       </Stack>
     </form>
-    )
-}
+  );
+};
 
 export default LoginForm;
--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -1,8 +1,20 @@
-import { FormControl, FormHelperText, IconButton, InputAdornment, InputLabel, Link, OutlinedInput, Stack } from "@mui/material";
+import {
+  FormControl,
+  FormHelperText,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  Link,
+  OutlinedInput,
+  Stack,
+} from "@mui/material";
 import { useCallback, useState } from "react";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
-import { IUserRegister, IValidationUserRegister } from "../../types/userInterfaces";
+import {
+  IUserRegister,
+  IValidationUserRegister,
+} from "../../types/userInterfaces";
 import { LoadingButton } from "@mui/lab";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { feedbackOffActionCreator } from "../../redux/features/uiSlice/uiSlice";
@@ -12,7 +24,9 @@ import { useNavigate } from "react-router-dom";
 
 const RegisterForm = (): JSX.Element => {
   const dispatch = useAppDispatch();
-  const {loading, feedback, statusCode} = useAppSelector((state) => state.ui);
+  const { loadingUser, feedback, statusCode } = useAppSelector(
+    (state) => state.ui
+  );
 
   const formInitialState: IUserRegister = {
     name: "",
@@ -20,12 +34,10 @@ const RegisterForm = (): JSX.Element => {
     username: "",
     password: "",
     userRol: "",
-  }
+  };
   const [formData, setFormData] = useState<IUserRegister>(formInitialState);
 
-  const submitRegisterForm = (
-    event: React.FormEvent<HTMLFormElement>
-  ) => {
+  const submitRegisterForm = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const isNotValidated = validateFields();
     if (isNotValidated) {
@@ -38,56 +50,57 @@ const RegisterForm = (): JSX.Element => {
 
   const resetData = (): void => {
     setFormData(formInitialState);
-  }
+  };
 
   const changeData = (event: React.ChangeEvent<HTMLInputElement>): void => {
     setFormData({ ...formData, [event.target.id]: event.target.value });
   };
 
-
   const errorInitialState: IValidationUserRegister = {
     name: false,
     username: false,
     password: false,
-  }
-  const [errors, setErrors] = useState<IValidationUserRegister>(errorInitialState);
+  };
+  const [errors, setErrors] =
+    useState<IValidationUserRegister>(errorInitialState);
 
   const validateFields = (): boolean => {
-    let tempErrors = {...errors};
+    let tempErrors = { ...errors };
     tempErrors.name = formData.name === "" ? true : false;
     tempErrors.username = formData.username === "" ? true : false;
     tempErrors.password = formData.password === "" ? true : false;
     setErrors(tempErrors);
 
-    return Object.values(tempErrors).some(element => element === true);
-  }
+    return Object.values(tempErrors).some((element) => element === true);
+  };
 
   const [showPassword, setShowPassword] = useState<boolean>(false);
 
   const handleClickShowPassword = () => {
-      setShowPassword(!showPassword);
+    setShowPassword(!showPassword);
   };
 
   const feedbackOff = useCallback(
-    () => dispatch(feedbackOffActionCreator())
-  , [dispatch]);
-  
+    () => dispatch(feedbackOffActionCreator()),
+    [dispatch]
+  );
+
   const userFound: IAlertCustom = {
     title: `Ya existe el usuario`,
     content: `
       Parece que el nombre de usuario que has indicado ya esta cogido.
       Prueba con otro o inicia sesión.`,
     type: "warning",
-    action: feedbackOff
-  }
+    action: feedbackOff,
+  };
 
   const userCreated: IAlertCustom = {
     title: `Usuario registrado`,
     content: `¡Genial! Ahora ya formas parte de Valencia Sin Gluten.
       Solo queda que inicies sesión y listo.`,
     type: "success",
-    action: feedbackOff
-  }
+    action: feedbackOff,
+  };
 
   const validationServer: IAlertCustom = {
     title: `Los datos introducidos no son correctos`,
@@ -95,8 +108,8 @@ const RegisterForm = (): JSX.Element => {
       El nombre, el apellido y el nombre de usuario no debe superar los 30 caracteres.
       La contraseña debe tener entre 5 y 20 caracteres.`,
     type: "error",
-    action: feedbackOff
-  }
+    action: feedbackOff,
+  };
 
   const navigate = useNavigate();
   const navigateToLogin = () => {
@@ -106,9 +119,10 @@ const RegisterForm = (): JSX.Element => {
   return (
     <form autoComplete="off" onSubmit={submitRegisterForm}>
       <Stack spacing={3}>
-
         <FormControl variant="outlined">
-          <InputLabel htmlFor="name" error={errors.name}>Nombre</InputLabel>
+          <InputLabel htmlFor="name" error={errors.name}>
+            Nombre
+          </InputLabel>
           <OutlinedInput
             id="name"
             type="text"
@@ -117,7 +131,9 @@ const RegisterForm = (): JSX.Element => {
             value={formData.name}
             error={errors.name}
           />
-          <FormHelperText id="name-helpertext" error={errors.name}>{errors.name ? "El nombre es obligatorio" : " "}</FormHelperText>
+          <FormHelperText id="name-helpertext" error={errors.name}>
+            {errors.name ? "El nombre es obligatorio" : " "}
+          </FormHelperText>
         </FormControl>
 
         <FormControl variant="outlined">
@@ -129,11 +145,13 @@ const RegisterForm = (): JSX.Element => {
             onChange={changeData}
             value={formData.surnames}
           />
-            <FormHelperText id="surname-helpertext">{" "}</FormHelperText>
+          <FormHelperText id="surname-helpertext"> </FormHelperText>
         </FormControl>
 
         <FormControl variant="outlined">
-          <InputLabel htmlFor="username" error={errors.username}>Username</InputLabel>
+          <InputLabel htmlFor="username" error={errors.username}>
+            Username
+          </InputLabel>
           <OutlinedInput
             id="username"
             type="text"
@@ -142,11 +160,15 @@ const RegisterForm = (): JSX.Element => {
             value={formData.username}
             error={errors.username}
           />
-          <FormHelperText id="username-helpertext" error={errors.username}>{errors.username ? "El nombre de usuario es obligatorio" : " "}</FormHelperText>
+          <FormHelperText id="username-helpertext" error={errors.username}>
+            {errors.username ? "El nombre de usuario es obligatorio" : " "}
+          </FormHelperText>
         </FormControl>
 
         <FormControl variant="outlined">
-          <InputLabel htmlFor="password" error={errors.password}>Password</InputLabel>
+          <InputLabel htmlFor="password" error={errors.password}>
+            Password
+          </InputLabel>
           <OutlinedInput
             id="password"
             role="password"
@@ -167,30 +189,69 @@ const RegisterForm = (): JSX.Element => {
             value={formData.password}
             error={errors.password}
           />
-          <FormHelperText id="password-helpertext" error={errors.password}>{errors.password ? "La contraseña es obligatoria" : " "}</FormHelperText>
+          <FormHelperText id="password-helpertext" error={errors.password}>
+            {errors.password ? "La contraseña es obligatoria" : " "}
+          </FormHelperText>
         </FormControl>
 
-        <LoadingButton fullWidth size="large" type="submit" variant="contained" loading={loading}>
+        <LoadingButton
+          fullWidth
+          size="large"
+          type="submit"
+          variant="contained"
+          loading={loadingUser}
+        >
           registrarse
         </LoadingButton>
-        
-        {feedback && 
-          (statusCode === 201 ? <AlertCustom title={userCreated.title} content={userCreated.content} type={userCreated.type} action={userCreated.action}></AlertCustom> : "")
-        }
 
-        {feedback && 
-          (statusCode === 409 ? <AlertCustom title={userFound.title} content={userFound.content} type={userFound.type} action={userFound.action}></AlertCustom> : "")
-        }
+        {feedback &&
+          (statusCode === 201 ? (
+            <AlertCustom
+              title={userCreated.title}
+              content={userCreated.content}
+              type={userCreated.type}
+              action={userCreated.action}
+            ></AlertCustom>
+          ) : (
+            ""
+          ))}
 
-        {feedback && 
-          (statusCode === 400 ? <AlertCustom title={validationServer.title} content={validationServer.content} type={validationServer.type} action={validationServer.action}></AlertCustom> : "")
-        }
+        {feedback &&
+          (statusCode === 409 ? (
+            <AlertCustom
+              title={userFound.title}
+              content={userFound.content}
+              type={userFound.type}
+              action={userFound.action}
+            ></AlertCustom>
+          ) : (
+            ""
+          ))}
 
-        <Link variant="subtitle2" type="button" component="button" underline="none" onClick={navigateToLogin}>¿Ya tienes una cuenta? ¡Inicia sesión!</Link>
-      
+        {feedback &&
+          (statusCode === 400 ? (
+            <AlertCustom
+              title={validationServer.title}
+              content={validationServer.content}
+              type={validationServer.type}
+              action={validationServer.action}
+            ></AlertCustom>
+          ) : (
+            ""
+          ))}
+
+        <Link
+          variant="subtitle2"
+          type="button"
+          component="button"
+          underline="none"
+          onClick={navigateToLogin}
+        >
+          ¿Ya tienes una cuenta? ¡Inicia sesión!
+        </Link>
       </Stack>
     </form>
-    )
-}
+  );
+};
 
 export default RegisterForm;
