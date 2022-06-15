de53e84a7d87be71c21a843bc6f0432f311b0a57
MariamLliso <contacto.mariamlliso@gmail.com>
2022-06-15 22:26:43 +0200

    tested navigation buttons

--- src/components/Layout/Navigation/Navigation.test.tsx
+++ src/components/Layout/Navigation/Navigation.test.tsx
@@ -6,8 +6,24 @@ import Navigation from "./Navigation";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "../../../redux/store/store";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-describe("Given a Navigation component page", () => {
+let mockLogged = true;
+
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useSelector: () => ({ logged: mockLogged }),
+}));
+
+const mockUseNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockUseNavigate,
+}));
+
+describe("Given a Navigation component", () => {
   describe("When it's invoked", () => {
     test("Then it should always match this snapshot", () => {
       const testedRegisterPage = TestRenderer.create(
@@ -23,6 +39,76 @@ describe("Given a Navigation component page", () => {
       );
 
       expect(testedRegisterPage).toMatchSnapshot();
+    });
+  });
+
+  describe("When it's invoked and user click at 'lugares' button", () => {
+    test("then it should call navigate with '/establishment/list'", () => {
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <ThemeProvider theme={theme}>
+              <GlobalStyle />
+              <CssBaseline />
+              <Navigation />
+            </ThemeProvider>
+          </Provider>
+        </BrowserRouter>
+      );
+
+      const expectedButton: HTMLButtonElement = screen.getByRole("button", {
+        name: "lugares",
+      });
+      userEvent.click(expectedButton);
+
+      expect(mockUseNavigate).toHaveBeenCalledWith("/establishment/list");
+    });
+  });
+
+  describe("When it's invoked and register user click at 'perfil' button", () => {
+    test("then it should call navigate with '/user/profile'", () => {
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <ThemeProvider theme={theme}>
+              <GlobalStyle />
+              <CssBaseline />
+              <Navigation />
+            </ThemeProvider>
+          </Provider>
+        </BrowserRouter>
+      );
+
+      const expectedButton: HTMLButtonElement = screen.getByRole("button", {
+        name: "perfil",
+      });
+      userEvent.click(expectedButton);
+
+      expect(mockUseNavigate).toHaveBeenCalledWith("/user/profile");
+    });
+  });
+
+  describe("When it's invoked and no register user click at 'perfil' button", () => {
+    test("then it should call navigate with '/login'", () => {
+      mockLogged = false;
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <ThemeProvider theme={theme}>
+              <GlobalStyle />
+              <CssBaseline />
+              <Navigation />
+            </ThemeProvider>
+          </Provider>
+        </BrowserRouter>
+      );
+
+      const expectedButton: HTMLButtonElement = screen.getByRole("button", {
+        name: "perfil",
+      });
+      userEvent.click(expectedButton);
+
+      expect(mockUseNavigate).toHaveBeenCalledWith("/login");
     });
   });
 });
